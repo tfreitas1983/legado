5dd659c447bb908c8c7e3e9cc3b64f6651ce643e
Thiago Freitas <63019037+tfreitas1983@users.noreply.github.com>
2021-10-26 16:47:54 -0300

    Update db.config.js

--- back-end/config/db.config.js
+++ back-end/config/db.config.js
@@ -1,19 +1,7 @@
 module.exports = {
-    /*HOST: "10.1.1.41",
+    HOST: "10.1.1.41",
     USER: "Admin",
-    PASSWORD: "Zidane",
-    DB: "legado",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }*/
-
-    HOST: "54.242.205.227",
-    USER: "rootAdmin",
-    PASSWORD: "cdrj1286",
+    PASSWORD: "password",
     DB: "legado",
     dialect: "mysql",
     pool: {
