10135e35b79336d3beb1e199359b54efea3584e8
Thiago Freitas <63019037+tfreitas1983@users.noreply.github.com>
2021-10-26 16:49:42 -0300

    Update http-common.js

--- front-end/http-common.js
+++ front-end/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-   //baseURL: "http://apihoramarcada.ddns.net:5099/api",
+   
    baseURL: "http://10.1.1.26:5099/api",
     headers: {
         "Content-type": "application/json"
